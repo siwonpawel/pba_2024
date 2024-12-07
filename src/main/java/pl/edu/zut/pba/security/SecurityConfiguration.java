b10512401c7511dcc16afa7bc874c2fd0d7599cb
Paweł Siwoń <siwonpawel@gmail.com>
2024-12-07 18:35:20 +0100

    lab05_z03

--- src/main/java/pl/edu/zut/pba/security/SecurityConfiguration.java
+++ src/main/java/pl/edu/zut/pba/security/SecurityConfiguration.java
@@ -1,7 +1,15 @@
 package pl.edu.zut.pba.security;
 
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -9,6 +17,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -36,14 +46,31 @@ public class SecurityConfiguration
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception
     {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
-                    auth
-                            .requestMatchers("/api/users").authenticated();
-                }).httpBasic(withDefaults())
+
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/api/users").authenticated())
+                .httpBasic(withDefaults())
+
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/users/{id}").hasAuthority("SCOPE_pba_resource"))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .decoder(jwtDecoder)))
                 .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(@Value("classpath:key.pem") Resource key) throws CertificateException, IOException
+    {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) cf.generateCertificate(key.getInputStream());
+        RSAPublicKey publicKey = (RSAPublicKey) cert.getPublicKey();
+
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
 }
